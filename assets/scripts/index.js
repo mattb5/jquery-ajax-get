358f33b212358880b3bddc8ead82f5c7ebdfa430
mattb5 <mattbuffardi@gmail.com>
2016-05-25 14:27:02 -0400

    second commit

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -8,7 +8,17 @@ const getBooksHandler = function (event) {
   libraryApi.getBooks().done(ui.onSuccess);
 };
 
+const getBookHandler = function (event) {
+  event.preventDefault();
+  libraryApi.getBooks().done(ui.onSuccessBook);
+};
 // On document ready
+//this is when you click the submit button
 $(() => {
   $('#get-books').on('click', getBooksHandler);
+});
+
+$(() => {
+  $('#get-book').on('click', getBookHandler);
+
 });
--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -4,6 +4,11 @@ const onSuccess = function (data) {
   console.table(data.books);
 };
 
+const onSuccessBook = function (data {
+  console.log(data.books[0]);
+};
+
 module.exports = {
   onSuccess,
+  onSuccessBook,
 }
